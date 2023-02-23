189058c4df6d3a87f2b50a92eef0465fb111acb8
Justin Murillo <murillo_justin@yahoo.com>
2023-02-22 22:29:00 -0800

    initial setup

--- sketch.js
+++ sketch.js
@@ -1,3 +1,15 @@
+/*********************************** 
+CMPM 169 TEAM PROJECT - CITY BUILDER
+
+Contributors:
+  Kenny Chau
+  Justin Murillo
+  Nathan Laureta
+  Vincent Kurniadjaja
+
+***********************************/    
+
+
 function setup() {
   createCanvas(400, 400);
 }
