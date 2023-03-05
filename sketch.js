189f888a6838df7b9f74eef97d8cbb6dbc44fa9e
CallMeCalvin101 <calvinkurniadjaja@gmail.com>
2023-03-04 17:35:13 -0800

    Merge of building and circle classes

--- sketch.js
+++ sketch.js
@@ -8,27 +8,98 @@ Contributors:
   Vincent Kurniadjaja
 
 ***********************************/
+// test variables
+let testB;
 let test;
 
+// Variables for Border
+let radius = 50;
+var isTheMousePressed = false;
+var bordorPlaced = false;
+
+// Created by Vincent, represents a single building
 class Building {
+  // Creates a building at x, y with max width and max height
   constructor (x, y, width, height) {
     this.x = x;
     this.y = y;
-    this.width = width;
-    this.height = height;
+    this.maxWidth = width
+    this.curWidth = 0;
+    this.maxHeight = height;
+    this.curHeight = 0;
   }
 
   draw() {
-    rect(this.x, this.y, this.width, this.height)
+    rect(this.x - this.curWidth / 2, this.y - this.curHeight, this.curWidth, this.curHeight)
+    this.tweenBuilding();
+  }
+
+  // This code will grow the building
+  tweenBuilding() {
+    let heightRate = this.maxHeight / 100
+    let widthRate = this.maxWidth / 100
+    if (this.curHeight < this.maxHeight) {
+      this.curHeight += heightRate;
+    }
+
+    if (this.curWidth < this.maxWidth) {
+      this.curWidth += widthRate
+    }
+  }
+}
+
+class Bordor {
+  constructor(x, y, radius) {
+        this.x =  x;
+        this.y = y;
+        this.radius = radius;
+  }
+    //update() {
+        //Compare all city's radius with this and check
+        //for each border in array
+        //if(radius doesn't collide with other radius) {
+            //Radius += growth
+        //}
+    //}
+  draw() {
+    stroke(255);
+    if(isTheMousePressed && bordorPlaced == false) {
+      fill(0)
+      ellipse(this.x, this.y, this.radius)
+      bordorPlaced = true
+    }
   }
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  test = new Building(width/2, height/2, 100, 400);
+  testB = new Building(width/2, height/2, 100, 400);
 }
 
 function draw() {
-  background(220);
+  //background(220);
+  testB.draw();
+  test = new Bordor(mouseX, mouseY, radius);
   test.draw();
+}
+
+function keyPressed() {
+  if(key == "1") {
+    radius = 50;
+  }
+  if(key == "2") {
+    radius = 100;
+  }
+  if(key == "3") {
+    radius = 150;
+  }
+}
+
+function mousePressed() {
+  isTheMousePressed = true;
+}
+
+function mouseReleased() {
+  isTheMousePressed = false;
+  bordorPlaced = false;
 }
