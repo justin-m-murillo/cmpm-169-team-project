63cc7df15be26559ef483554bc7c00c02605c2bf
CallMeCalvin101 <calvinkurniadjaja@gmail.com>
2023-03-04 17:14:15 -0800

    Building Class

--- sketch.js
+++ sketch.js
@@ -7,13 +7,28 @@ Contributors:
   Nathan Laureta
   Vincent Kurniadjaja
 
-***********************************/    
+***********************************/
+let test;
 
+class Building {
+  constructor (x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  draw() {
+    rect(this.x, this.y, this.width, this.height)
+  }
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  test = new Building(width/2, height/2, 100, 400);
 }
 
 function draw() {
   background(220);
+  test.draw();
 }
